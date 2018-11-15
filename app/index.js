45aa5b97fd38bb70254535a1a06eef2baf92f3ff
psc-kamei <info.pscreator@gmail.com>
2018-11-15 13:49:32 +0900

    init

---
+++ app/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+function start() {
+  app.listen(4000);
+}
+
+console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+module.exports = app
+module.exports = start
